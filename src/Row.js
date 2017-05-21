b0421f88c43103b8c7bfe1b17b2b09a418e890a9
Aaron <AaronBatilo@gmail.com>
2017-05-21 07:11:56 -0600

    Made a row component of sorts

---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Row.css'
+
+class Row extends Component {
+  render() {
+    return (
+      <div className="Row">
+        <div className="Row-title">Software Engineer</div>
+        <div className="Row-subtitle">Comcast</div>
+      </div>
+    );
+  }
+}
+
+export default Row
