927c3fd8e1170630385746b522915e6018a900f3
Aaron <AaronBatilo@gmail.com>
2017-05-29 21:15:30 -0600

    Prefixing react-app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
-const BASE_URL = runtimeEnv().BASE_URL || 'http://localhost:8080/jobs';
+const BASE_URL = runtimeEnv().REACT_APP_BASE_URL || 'http://localhost:8080/jobs';
 
 ReactDOM.render(
   <App base={BASE_URL}/>,
