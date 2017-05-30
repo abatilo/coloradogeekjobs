06265f15b22a302545302946bc16fe8512bdf3b8
Aaron <AaronBatilo@gmail.com>
2017-05-29 21:07:24 -0600

    It's going to work this time

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
-const BASE_URL = runtimeEnv().env.BASE_URL || 'http://localhost:8080/jobs';
+const BASE_URL = runtimeEnv().BASE_URL || 'http://localhost:8080/jobs';
 
 ReactDOM.render(
   <App base={BASE_URL}/>,
