10d97bd67f1692b14ae2703696b981122f8ef1d8
Aaron <AaronBatilo@gmail.com>
2017-06-11 14:18:03 -0600

    Now switching routes will clear the current content. Just needed the exact keyword

--- src/App.js
+++ src/App.js
@@ -4,27 +4,15 @@ import Post from './Post.js';
 import Home from './Home.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props.get);
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/post'>Post</Link></li>
-          </ul>
-          <hr/>
-          <Route path='/' render = {(props) => (<Home get={this.props.get} />)}/>
-          <Route path='/post' component={Post}/>
-        </div>
-      </Router>
-    );
-  }
+function App(props) {
+  return (
+    <Router>
+      <div>
+        <Route exact path='/' render={() => <Home get={props.get} />}/>
+        <Route path='/post' component={Post}/>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
     <div className="Header">
       <p>This site is <b>heavily</b> inspired by <a href="oldgeekjobs.com">oldgeekjobs.com</a></p>
+      <Link to='/post'>Post a job. $10 for 2 weeks.</Link>
     </div>
   );
 }
