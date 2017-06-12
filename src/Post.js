d835c70a25d56700a0211983248eb00c4140d112
Aaron <AaronBatilo@gmail.com>
2017-06-11 22:04:51 -0600

    Set remote to false by default

--- src/Post.js
+++ src/Post.js
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class Post extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      remote: false
+    };
     // Bind all the handlers
     this.handleCity = this.handleCity.bind(this);
     this.handleCompany = this.handleCompany.bind(this);
