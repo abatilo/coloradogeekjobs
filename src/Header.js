37743cea0c9d37d53a5a47bdb4611f2336958950
Aaron <AaronBatilo@gmail.com>
2017-06-13 20:03:14 -0600

    Fixed the link to oldgeekjobs.com

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header(props) {
   return (
     <div className="Header">
-      <p>This site is <b>heavily</b> inspired by <a href="oldgeekjobs.com">oldgeekjobs.com</a></p>
+      <p>This site is <b>heavily</b> inspired by <a href="https://www.oldgeekjobs.com">oldgeekjobs.com</a></p>
       <Link to='/post'>Post a job. $10 for 2 weeks.</Link>
     </div>
   );
