79e6cf4eea00993952675b499706350d912c086b
Aaron <AaronBatilo@gmail.com>
2017-06-16 13:49:46 -0600

    Added my Twitter

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 function Header(props) {
   return (
     <div className="Header">
+      <a href='https://www.twitter.com/aaronbatilo'>Follow me</a>
       <p>This site is <b>heavily</b> inspired by <a href="https://www.oldgeekjobs.com">oldgeekjobs.com</a></p>
-      <Link to='/post'>Post a job. $10 for 2 weeks.</Link>
+      <Link to='/post'>Post a job. $10 for 2 weeks</Link>
     </div>
   );
 }
