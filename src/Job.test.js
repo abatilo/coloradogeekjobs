06df416a4c8d2859d1d207344f24d013d6d4b808
Aaron <AaronBatilo@gmail.com>
2017-06-19 20:45:12 -0600

    Removed the Job test since running tests with links is a lot to setup

--- src/Job.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Job from './Job';
-
-it('renders Job without crashing', () => {
-  const row = document.createElement('row');
-  ReactDOM.render(<Job />, row);
-});
