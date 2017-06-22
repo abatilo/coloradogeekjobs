51168f215c813bb1d878d2fa69178c4624384a3d
Aaron <AaronBatilo@gmail.com>
2017-06-21 20:12:44 -0600

    Made it so that a selected job will pass its information into a new component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import 'whatwg-fetch';
-import React, { Component } from 'react';
 import Header from './Header.js';
 import Job from './Job.js';
+import React, { Component } from 'react';
+import SelectedJob from './SelectedJob.js';
 
 class Home extends Component {
   constructor(props) {
@@ -9,7 +10,14 @@ class Home extends Component {
     // Set state to be empty
     this.state = {
       jobs: [],
-      selectedJob: ''
+      selectedJobCity: '',
+      selectedJobCompany: '',
+      selectedJobDescription: '',
+      selectedJobEmail: '',
+      selectedJobHow: '',
+      selectedJobRemote: '',
+      selectedJobTitle: '',
+      selectedJobUrl: ''
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -23,11 +31,23 @@ class Home extends Component {
   }
 
   handleClick(id) {
+    /*
+     * This for loop might be slow. The question is if we'll
+     * ever get enough job postings that it will actually be slow
+     */
     for (let i = 0; i < this.state.jobs.length; ++i) {
       let job = this.state.jobs[i];
-      console.log(job);
       if (job.id === id) {
-        this.setState({ selectedJob: job.title });
+        this.setState({
+          selectedJobCity: job.city,
+          selectedJobCompany: job.company,
+          selectedJobDescription: job.description,
+          selectedJobEmail: job.email,
+          selectedJobHow: job.how,
+          selectedJobRemote: job.remote,
+          selectedJobTitle: job.title,
+          selectedJobUrl: job.url
+        });
       }
     }
   }
@@ -40,9 +60,16 @@ class Home extends Component {
           this.state.jobs.map((job) =>
             <Job click={this.handleClick} key={job.id} id={job.id} title={job.title} company={job.company} date={job.date}/>)
         }
-        <div>
-          {this.state.selectedJob}
-        </div>
+        <SelectedJob
+          city={this.state.selectedJobCity}
+          company={this.state.selectedJobCompany}
+          description={this.state.selectedJobDescription}
+          email={this.state.selectedJobEmail}
+          how={this.state.selectedJobHow}
+          remote={this.state.selectedJobRemote}
+          title={this.state.selectedJobTitle}
+          url={this.state.selectedJobUrl}
+        />
       </div>
     );
   }
---
+++ src/SelectedJob.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+function SelectedJob(props) {
+  return (
+    <div>
+      <p>{props.city}</p>
+      <p>{props.company}</p>
+      <p>{props.description}</p>
+      <p>{props.email}</p>
+      <p>{props.how}</p>
+      <p>{props.remote}</p>
+      <p>{props.title}</p>
+      <p>{props.url}</p>
+    </div>
+  );
+}
+
+export default SelectedJob;
