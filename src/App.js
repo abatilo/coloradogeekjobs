d3457fa7a7a1948812ef786119ce55594e68794c
Aaron <AaronBatilo@gmail.com>
2017-06-21 20:17:20 -0600

    Deleted Detail.js since I'm no longer doing a different page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { Component } from 'react';
-import Detail from './Detail.js';
 import Home from './Home.js';
 import Post from './Post.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -11,7 +10,6 @@ function App(props) {
       <div>
         <Route exact path='/' render={() => <Home get={props.get} />}/>
         <Route path='/post' render={() => <Post post={props.post} />}/>
-        <Route path='/job/:id' component={Detail} />
       </div>
     </Router>
   );
--- src/SelectedJob.js
+++ src/SelectedJob.js
@@ -1,8 +1,9 @@
+import './SelectedJob.css';
 import React, { Component } from 'react';
 
 function SelectedJob(props) {
   return (
-    <div>
+    <div className='SelectedJob'>
       <p>{props.city}</p>
       <p>{props.company}</p>
       <p>{props.description}</p>
