e020395c634280294995ac087c71a2330c503d21
Aaron <AaronBatilo@gmail.com>
2017-07-02 16:01:29 -0600

    Kind of understanding how to style things?

--- src/Home.js
+++ src/Home.js
@@ -54,22 +54,32 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Header />
-        {
-          this.state.jobs.map((job) =>
-            <Job click={this.handleClick} key={job.id} id={job.id} title={job.title} company={job.company} date={job.date}/>)
-        }
-        <SelectedJob
-          city={this.state.selectedJobCity}
-          company={this.state.selectedJobCompany}
-          description={this.state.selectedJobDescription}
-          email={this.state.selectedJobEmail}
-          how={this.state.selectedJobHow}
-          remote={this.state.selectedJobRemote}
-          title={this.state.selectedJobTitle}
-          url={this.state.selectedJobUrl}
-        />
+        <table className='App-table'>
+          <tr>
+          <td className='App-table-column'>
+          <div className='.Job-list'>
+            {
+              this.state.jobs.map((job) =>
+                <Job click={this.handleClick} key={job.id} id={job.id} title={job.title} company={job.company} date={job.date}/>)
+            }
+          </div>
+          </td>
+          <td className='App-table-column'>
+          <SelectedJob className='.SelectedJob'
+            city={this.state.selectedJobCity}
+            company={this.state.selectedJobCompany}
+            description={this.state.selectedJobDescription}
+            email={this.state.selectedJobEmail}
+            how={this.state.selectedJobHow}
+            remote={this.state.selectedJobRemote}
+            title={this.state.selectedJobTitle}
+            url={this.state.selectedJobUrl}
+          />
+          </td>
+          </tr>
+        </table>
       </div>
     );
   }
