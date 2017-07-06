f22a1d5d7c93c1deb8aa86bb76912d1680d5717b
Aaron <AaronBatilo@gmail.com>
2017-07-06 09:50:57 -0600

    Made the website say that jobs are free to post, at least for now

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
     <div className="Header">
       <a href='https://www.twitter.com/aaronbatilo'>Follow me</a>
       <p>This site is <b>heavily</b> inspired by <a href="https://www.oldgeekjobs.com">oldgeekjobs.com</a></p>
-      <Link to='/post'>Post a job. $20 for 30 days</Link>
+      <Link to='/post'>Post a job. <b>Free</b> for 30 days</Link>
     </div>
   );
 }
