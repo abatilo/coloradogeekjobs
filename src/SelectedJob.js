10eac95bed805dd60489a468d8858d2b7588fdd1
Aaron <AaronBatilo@gmail.com>
2017-09-04 19:50:28 -0600

    Integrate SmartText package
    We want email addresses to be easily clickable. The fastest way
    I could find to do that was to use the SmartText plugin
    which scans the text with a regex for an email and replaces
    it with a custom defined Component

--- src/SelectedJob.js
+++ src/SelectedJob.js
@@ -1,11 +1,26 @@
 import './SelectedJob.css';
 import React, { Component } from 'react';
+import SmartText from 'react-smart-text'
+
+// This is what we're hunting in the text.
+const emailRegex = /\w+@.+?\.(com)/g
+
+// This is what we want to replace matches with.
+const Email = (props) =>
+  <a href={`mailto:${props.text}`}>{props.text}</a>
 
 function SelectedJob(props) {
   return (
     <div className='SelectedJob'>
-      <p><b>Job description:</b><br/>{props.description}</p>
-      <p><b>How to apply:</b><br/>{props.how}</p>
+      <label for='description' id='descriptionLabel'>Description</label>
+      <SmartText id='description' name='description' regex={emailRegex} component={Email}>
+        {props.description}
+      </SmartText>
+      <br/>
+      <label for='how' id='howLabel'>How to apply</label>
+      <SmartText id='how' name='how' regex={emailRegex} component={Email}>
+        {props.how}
+      </SmartText>
     </div>
   );
 }
