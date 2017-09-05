2967d86762edf2bfc8f849d046e814c417d9438f
Aaron <AaronBatilo@gmail.com>
2017-09-04 19:05:21 -0600

    Remove redudant information in selected job details

--- src/SelectedJob.js
+++ src/SelectedJob.js
@@ -4,10 +4,6 @@ import React, { Component } from 'react';
 function SelectedJob(props) {
   return (
     <div className='SelectedJob'>
-      <p><b>Job title:</b><br/>{props.title}</p>
-      <p><b>City:</b><br/>{props.city}</p>
-      <p><b>Company:</b><br/>{props.company}</p>
-      <p><b>Website:</b><br/>{props.url}</p>
       <p><b>Job description:</b><br/>{props.description}</p>
       <p><b>How to apply:</b><br/>{props.how}</p>
     </div>
